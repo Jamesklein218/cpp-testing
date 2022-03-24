85467f8ba2e60fa34326e448dde24b956c4768f4
jamesklein218 <jamesklein218@gmail.com>
2022-03-25 00:03:40 +0700

    Example cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
 
 using namespace std;
 
-int m, n;
+int t;
 
-int searchPath(int arr[255][255], int i, int j, int step) {
-  step++;
-  if (i + 1, )
-  return min(searchPath(arr, i + 1, j, step + 1), searchPath(arr, i, j + 1, step + 1));
-}
+// TODO
 
 int main() {
-  cin >> m;
-  cin >> n;
-  int arr[255][255];
-  for (int i = 0; i < m; ++i) {
-    for (int j = 0; j < n; ++j) {
-      cin >> arr[i][j];
-    }
+  // Input 1
+  for (int i = 0; i < t; ++i) {
+    // Input 2
+    cout << "Case #" << i + 1 << ": " << 
+    "TODO"
+    << endl;
   }
-
-  cout << searchPath(arr, 0, 0, 0);
   return 0;
 }
